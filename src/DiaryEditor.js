b0e45f5040f2e6c97fb8be3ca4e48d9c549c695b
youngcheolLee <zodiac0905@gmail.com>
2022-10-24 21:15:01 +0900

    [feat] useRef이용하여 focus 기능 추가

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 /* [설명]
    - 다이어리 "작성" 기능을 담당하는 컴포넌트
 */
 const DiaryEditor = () => {
+  //useRef함수를 호출하여 어떠한 반환값을 authorInput 이란 상수에 담아줌
+  //useRef()는 DOM요소를 선택가능하게 하는 기능
+  const authorInput = useRef();
+  const contentInput = useRef();
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -19,6 +23,19 @@ const DiaryEditor = () => {
 
   const handleSubmit = (e) => {
     console.log(state);
+    if (state.author.length < 1) {
+      alert("작성자명을 입력해 주세요!");
+      authorInput.current.focus();
+      //current = 현재가르키는 값. 즉 authorInput.current = authorInput 태그 이고, focus()를 통해 포커스 기능을 넣어줌
+      return;
+    }
+
+    if (state.content.length < 4) {
+      alert("일기의 본문을 3글자 이상 입력해주세요!");
+      contentInput.current.focus();
+      return;
+    }
+
     alert("일기 저장성공!");
   };
 
@@ -27,6 +44,8 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          //ref 속성에 에 authorInput을 담아 해당 input태그에 접근이 가능하다.
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -35,6 +54,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}

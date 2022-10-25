0a91abbd9b3b45a7c98a53280f3b0ca9115fcaa6
youngcheolLee <zodiac0905@gmail.com>
2022-10-25 23:30:50 +0900

    [feat]다이어리 List, Item 페이지 추가

---
+++ src/DiaryList.js
@@ -0,0 +1,24 @@
+import DiaryItem from "./DiaryItem.js";
+
+const DiaryList = ({ diaryList }) => {
+  console.log(diaryList);
+  return (
+    <div className="DiaryList">
+      <h4>{diaryList.length} 개의 일기가 있습니다!</h4>
+
+      {/* Props로 받은 diaryList를 배열로 랜더링 하는 구문 */}
+      <div>
+        {diaryList.map((item) => (
+          <DiaryItem key={item.id} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+//예상외의 Props가 전달 되었다면 해당 구문이 실행하며 빈 배열이 노출함
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
